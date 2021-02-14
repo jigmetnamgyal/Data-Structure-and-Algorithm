b8af92e9416c6b72530d735d1cc4e07779bda2f2
JigmeT12 <jigmetashi02@gmail.com>
2021-02-14 11:40:09 +0400

    FEAT: lcm added

---
+++ practisecpp/another.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Jigme Tashi on 2/13/21.
+//
+
--- practisecpp/main.cpp
+++ practisecpp/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
-int multiply(int, int);
-
+void swap(int &a, int &b){
+    int temp = 0;
+    temp = a;
+    a = b;
+    b = temp;
+}
 int main() {
-    double a = 65;
-    std::cout<< sizeof a << std::endl;
+    int a, b;
+    std::cin >> a >> b;
+    swap(a,b);
+    std::cout << "a value is: " << a << " The value of b is: " << b <<std::endl;
     return 0;
 }
