515756d4a74bd5f0c2963c0e011e32ed56d29914
JigmeT12 <jigmetashi02@gmail.com>
2021-02-15 16:05:23 +0400

    commit dsa

--- practisecpp/sieveOfEratostheness.cpp
+++ practisecpp/sieveOfEratostheness.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 using namespace std;
+//Finding prime number using trial and division method
+//bool isPrime(int n){
+//    for(int i = 2; i <= sqrt(n); i++){
+//        if (n % i == 0) return false;
+//    }
+//    return true;
+//
+//}
 
-bool isPrime(int n){
-    for(int i = 2; i <= sqrt(n); i++){
-        if (n % i == 0) return false;
+//finding prime number using sieve of Eratostheness. _ we list a number from 2 to n (and initially say all of them are prime)
+
+void sieve(int n){
+    int prime[100] = {0};
+    for(int i = 2; i <= n; i++){
+        if(prime[i] == 0){
+            for(int j = i*i; j <=n; j+=i){
+                prime[j] = 1;
+            }
+        }
     }
-    return true;
-
+    for(int i = 2; i <=n; i++){
+        if(prime[i]==0){
+            cout << i << endl;
+        }
+    }
+    
 }
 int main(){
-    cout << isPrime(7) << endl;
+    sieve(10);
 }
+
+    
