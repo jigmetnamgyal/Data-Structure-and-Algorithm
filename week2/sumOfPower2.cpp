d7228c615016efa0ba9388e5de2fb37e0dd29d7e
JigmeT12 <jigmetashi02@gmail.com>
2021-02-20 19:07:15 +0400

    ADDED: correct powers of 2 sum

--- week2/sumOfPower2.cpp
+++ week2/sumOfPower2.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 typedef long long ll;
 
-ll exp(ll x, ll n){
+ll exp(ll x, ll n, ll m){
     if(n == 0) return 1;
-    if(n % 2 == 0) return exp(x * x,n/2);
-    return x * exp(x, n-1);
+    if(n % 2 == 0) return exp((x * x) % m,n/2, m);
+    return (x * exp(x, n-1, m)) % m;
 }
-ll sumOfPowersTwo(ll n, ll m){
-    ll sum = 0;
-    for(int i = 1; i <= n; i++){
-        sum += exp(i,i) % m;
-    }
-    return sum;
-}
+
 int main(){
-    ll n, m;
+    ll n, m, sum = 0;
     std::cin >> n >> m;
-    std::cout << sumOfPowersTwo(n,m) << std::endl;
+    for(int i = 1; i <= n; i++){
+        sum = ((sum + exp(i,i, m)) % m);
+    }
+    std::cout << sum << std::endl;
 }
