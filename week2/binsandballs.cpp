5c0f0250863db434ed754ea3e83e09188c2da991
JigmeT12 <jigmetashi02@gmail.com>
2021-02-20 19:43:43 +0400

    ADDED: full answer for bins and balls

--- week2/binsandballs.cpp
+++ week2/binsandballs.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 typedef long long ll;
+#define MOD 1000000007
 
-ll exp(ll x, ll n){
+ll exp(ll x, ll n, ll m){
     if (n == 0) return 1;
-    if(n % 2 == 0) return exp(x * x, n/2);
-
-    return x * exp(x, n-1);
+    if(n % 2 == 0) return exp((x * x) % m,n/2,m);
+    return (x * exp(x, n-1, m)) % m;
 }
-
 int main(){
-    ll x;
-
-    std::cin >> x;
-    std::cout << (x * exp(x-1, x-1)) % (exp(10, 9) + 7) << std::endl;
+    ll n;
+    std::cin >> n;
+    std::cout << ((n * exp(n-1, n-1, MOD)) % MOD) << std::endl;
 }
